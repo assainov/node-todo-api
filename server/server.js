dc8169a0df7fcc688cd1c159a033f2be7f6588ad
Ilyas Assainov <iliyas.assainov@gmail.com>
2018-11-21 23:32:25 +0400

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
     .catch((err) => res.status(400).send(err));
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find()
+    .then((todos) => {
+        res.send({todos})
+    })
+    .catch((err) => {
+        res.status(400).send(err);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is running at port 3000');
 });
