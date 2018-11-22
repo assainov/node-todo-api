065d4e26fed80cfa460d2aa0f878206609392278
Ilyas Assainov <iliyas.assainov@gmail.com>
2018-11-23 01:04:15 +0400

    add PATCH /todos/:id

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -15,3 +15,6 @@ const {User} = require('../server/models/user');
 Todo.findOneAndDelete({_id: ObjectID('5bf6af96d9d85320ace0af01')})
 .then((doc) => console.log('removed todo:', doc))
 .catch((err) => console.log(err));
+
+// Todo.findByIdAndRemove(id).then...
+
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const {ObjectID} = require('mongodb');
 const bodyParser = require('body-parser');
@@ -68,6 +69,34 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send(err);
     })
     
+});
+
+app.patch('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.competed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then(todo => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    })
+    .catch(err => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
